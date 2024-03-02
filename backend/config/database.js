5535b3af9beae1e0281d6462621149be032296c4
Soni Karm <93865733+karm1000@users.noreply.github.com>
2024-03-02 17:59:54 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.
-    connect("mongodb://127.0.0.1:27017/webtech-ecomm")
+    connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Mongo connection successful !!");
     })
